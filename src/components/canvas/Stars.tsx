da82c697be0dd92459ceb160e4c1873c68d607bf
Hongyu.Zhao <nathan_115210@hotmail.com>
2024-09-09 05:55:54 +0300

    fix Stars position property type

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,15 +1,15 @@
-import { useState, useRef, Suspense } from "react";
+import { PointMaterial, Points, Preload } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random";
+import { Suspense, useRef, useState } from "react";
 
 const Stars = () => {
   const ref = useRef<THREE.Points | null>(null);
 
-  const [sphere] = useState<Float32Array | Float64Array>(() => {
+  const [sphere] = useState<Float32Array>(() => {
     const box = random.inBox(new Float32Array(10_000), { sides: [1, 2, 1] });
-
-    return random.inSphere(box.slice(0), { radius: 0.75 });
+  
+    return random.inSphere(box.slice(0), { radius: 0.75 }) as Float32Array;
   });
 
   useFrame((state, delta) => {
