3f99a03902514f83e382eea0274fafbaea0415f7
Hongyu.Zhao <nathan_115210@hotmail.com>
2024-12-01 02:12:45 +0200

    remove the unused code

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-const { a, b }: { a: string; b: number } = ((list: string) => {
-  if (list === "hello") {
-    return { a: "hello", b: 42 };
-  }
-  return { a: "hello", b: 42 };
-})();
