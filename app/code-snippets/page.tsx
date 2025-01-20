d251c5cdb7765a421619d6b87ed0022bfd7162cb
Hongyu.Zhao <nathan_115210@hotmail.com>
2025-01-20 03:20:42 +0200

    feat(code-snippets): refactor the code snippets card component and adding more demos

--- app/code-snippets/page.tsx
+++ app/code-snippets/page.tsx
@@ -1,14 +1,56 @@
+import { Card, LayoutGrid } from "@/components/ui/layout-grid";
+import React from "react";
 import { codeSnippets } from "@/data";
-import RecentProjects, { ProjectsProps } from "@/components/RecentProjects";
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "@/components/MagicButton";
+
+export interface DemoItemProps {
+  title: string;
+  description: string;
+  techList?: string;
+  thumbnail: string;
+  link: string;
+}
+
+const demoItem = ({ title, description, techList, link }: DemoItemProps) => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">{title}</p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        {description}
+      </p>
+      {techList && (
+        <p className="font-normal text-base text-amber-500">{techList}</p>
+      )}
+      <a href={link} target="_blank">
+        <MagicButton
+          title="Check it out"
+          icon={<FaLocationArrow />}
+          position="right"
+        />
+      </a>
+    </div>
+  );
+};
+
+const demoData: Card[] = codeSnippets.map((item, index) => {
+  return {
+    id: index + 1,
+    content: demoItem(item),
+    className: `md:col-span-1`,
+    thumbnail: item.thumbnail,
+  };
+});
 
 const page = () => {
-  const codeSnippetsData: ProjectsProps = {
-    heading: "Code snippets",
-    description:
-      "These code snippets are either inspired by challenges from my daily work or by other awesome developers online. Mainly for fun and exploring more potential, fancy things.",
-    items: codeSnippets,
-  };
-  return <RecentProjects {...codeSnippetsData} />;
+  return (
+    <div className="h-screen py-20 w-full mt-16">
+      <h2 className="heading">
+        <span className="text-purple">Code snippets</span>
+      </h2>
+      <LayoutGrid cards={demoData} />
+    </div>
+  );
 };
 
 export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,17 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import Footer from "@/components/Footer";
 import { navItems } from "@/data";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import type { Metadata } from "next";
+import { getMetadata } from "@/lib/getMetadata";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://zhaohongyu.netlify.app"),
-  title: "Zhao Hongyu | Portfolio",
-  description: "Zaho Hongyu's portfolio",
-  applicationName: "Zhao Hongyu's portfolio",
-  authors: [{ name: "Zhao Hongyu", url: "https://zhaohongyu.netlify.app" }],
-  generator: "Next.js, React, TailwindCSS",
-  keywords: ["Zhao Hongyu", "Portfolio", "React", "Next.js"],
-  openGraph: {
-    type: "website",
-    title: "Zhao Hongyu | Portfolio",
-    description: "Zhao Hongyu's portfolio website",
-    url: "https://zhaohongyu.netlify.app",
-    siteName: "Zhao Hongyu | Portfolio",
-    images: [
-      {
-        url: "https://zhaohongyu.netlify.app/og-image.jpg",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://zhaohongyu.netlify.app/og-image.jpg",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-  },
-  icons: {
-    icon: "https://zhaohongyu.netlify.app/favicon.ico",
-  },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return await getMetadata();
+}
 
 export default function RootLayout({
   children,
